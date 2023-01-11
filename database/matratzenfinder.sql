668f08b87ddc76684e2c3a5673a427c99c05fa57
sugii <cindy.hinze2001@gmail.com>
2023-01-11 13:46:47 +0100

    befüllte datenbank (20)

--- database/matratzenfinder.sql
+++ database/matratzenfinder.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 09. Jan 2023 um 15:41
--- Server-Version: 10.4.25-MariaDB
--- PHP-Version: 8.1.10
+-- Erstellungszeit: 11. Jan 2023 um 13:45
+-- Server-Version: 10.4.24-MariaDB
+-- PHP-Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,8 +39,7 @@ CREATE TABLE `allergy` (
 INSERT INTO `allergy` (`a_id`, `allergy_type`) VALUES
 (1, 'Latex'),
 (2, 'Milben'),
-(3, 'Federn'),
-(4, 'Tierhaar');
+(3, 'Tierhaar');
 
 -- --------------------------------------------------------
 
@@ -50,17 +49,64 @@ INSERT INTO `allergy` (`a_id`, `allergy_type`) VALUES
 
 CREATE TABLE `allergy_mattress` (
   `am_id` int(11) NOT NULL,
-  `a_id` int(11) DEFAULT NULL,
-  `ma_id` int(11) DEFAULT NULL
+  `ma_id` int(11) NOT NULL,
+  `a_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Daten für Tabelle `allergy_mattress`
 --
 
-INSERT INTO `allergy_mattress` (`am_id`, `a_id`, `ma_id`) VALUES
-(1, 1, 8),
-(2, 1, 12);
+INSERT INTO `allergy_mattress` (`am_id`, `ma_id`, `a_id`) VALUES
+(1, 1, 1),
+(2, 1, 3),
+(3, 1, 2),
+(4, 2, 1),
+(5, 2, 2),
+(6, 2, 3),
+(7, 3, 1),
+(8, 3, 2),
+(9, 3, 3),
+(10, 4, 2),
+(11, 4, 3),
+(12, 5, 1),
+(13, 5, 2),
+(14, 5, 3),
+(15, 6, 1),
+(16, 6, 2),
+(17, 6, 3),
+(18, 7, 2),
+(19, 7, 3),
+(20, 8, 2),
+(21, 9, 2),
+(22, 9, 3),
+(23, 10, 2),
+(24, 10, 3),
+(25, 11, 2),
+(26, 11, 3),
+(27, 12, 1),
+(28, 12, 2),
+(29, 12, 3),
+(30, 13, 1),
+(31, 13, 2),
+(32, 13, 3),
+(33, 14, 2),
+(34, 14, 3),
+(35, 15, 2),
+(36, 15, 3),
+(37, 16, 1),
+(38, 16, 2),
+(39, 16, 3),
+(40, 17, 2),
+(41, 17, 3),
+(42, 18, 2),
+(43, 18, 3),
+(44, 19, 1),
+(45, 19, 2),
+(46, 19, 3),
+(47, 20, 1),
+(48, 20, 2),
+(49, 20, 3);
 
 -- --------------------------------------------------------
 
@@ -78,8 +124,9 @@ CREATE TABLE `complaints` (
 --
 
 INSERT INTO `complaints` (`c_id`, `complaints`) VALUES
-(1, 'Rücken'),
-(2, 'Schultern');
+(1, 'Ruecken'),
+(2, 'Schultern'),
+(3, 'Huefte');
 
 -- --------------------------------------------------------
 
@@ -89,35 +136,149 @@ INSERT INTO `complaints` (`c_id`, `complaints`) VALUES
 
 CREATE TABLE `complaints_mattress` (
   `cm_id` int(11) NOT NULL,
-  `c_id` int(11) NOT NULL,
-  `ma_id` int(11) NOT NULL
+  `ma_id` int(11) NOT NULL,
+  `c_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Daten für Tabelle `complaints_mattress`
 --
 
-INSERT INTO `complaints_mattress` (`cm_id`, `c_id`, `ma_id`) VALUES
-(1, 1, 8);
+INSERT INTO `complaints_mattress` (`cm_id`, `ma_id`, `c_id`) VALUES
+(1, 1, 2),
+(2, 1, 3),
+(3, 1, 1),
+(4, 2, 1),
+(5, 2, 2),
+(6, 2, 3),
+(7, 3, 3),
+(8, 4, 1),
+(9, 4, 2),
+(10, 4, 3),
+(11, 5, 1),
+(12, 5, 2),
+(13, 5, 3),
+(14, 6, 1),
+(15, 6, 2),
+(16, 6, 3),
+(17, 16, 1),
+(18, 16, 2),
+(19, 16, 3),
+(20, 17, 1),
+(21, 17, 2),
+(22, 17, 3),
+(23, 18, 1),
+(24, 18, 2),
+(25, 18, 3),
+(26, 19, 3),
+(27, 20, 2);
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `group`
+-- Tabellenstruktur für Tabelle `groups`
 --
 
-CREATE TABLE `group` (
+CREATE TABLE `groups` (
   `g_id` int(11) NOT NULL,
-  `group_name` varchar(255) NOT NULL
+  `groups_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Daten für Tabelle `group`
+-- Daten für Tabelle `groups`
 --
 
-INSERT INTO `group` (`g_id`, `group_name`) VALUES
+INSERT INTO `groups` (`g_id`, `groups_name`) VALUES
 (1, 'Erwachsene'),
 (2, 'Kinder');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `haertegrade`
+--
+
+CREATE TABLE `haertegrade` (
+  `h_id` varchar(50) NOT NULL,
+  `haertegrad` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `haertegrade`
+--
+
+INSERT INTO `haertegrade` (`h_id`, `haertegrad`) VALUES
+('H1', 'soft'),
+('H2', 'medium'),
+('H3', 'fest'),
+('H4', 'extra fest');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `haertegrade_mattress`
+--
+
+CREATE TABLE `haertegrade_mattress` (
+  `hm_id` int(11) NOT NULL,
+  `ma_id` int(11) NOT NULL,
+  `h_id` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `haertegrade_mattress`
+--
+
+INSERT INTO `haertegrade_mattress` (`hm_id`, `ma_id`, `h_id`) VALUES
+(2, 1, 'H1'),
+(3, 1, 'H2'),
+(4, 1, 'H3'),
+(5, 2, 'H1'),
+(6, 2, 'H2'),
+(7, 2, 'H3'),
+(8, 3, 'H1'),
+(9, 3, 'H2'),
+(10, 3, 'H3'),
+(11, 3, 'H4'),
+(12, 4, 'H1'),
+(13, 4, 'H2'),
+(14, 4, 'H3'),
+(15, 5, 'H1'),
+(16, 5, 'H2'),
+(17, 5, 'H3'),
+(18, 6, 'H1'),
+(19, 6, 'H2'),
+(20, 6, 'H3'),
+(21, 7, 'H2'),
+(22, 8, 'H1'),
+(23, 8, 'H2'),
+(24, 8, 'H3'),
+(25, 9, 'H2'),
+(26, 9, 'H3'),
+(27, 10, 'H2'),
+(28, 11, 'H2'),
+(29, 12, 'H2'),
+(31, 13, 'H2'),
+(32, 14, 'H1'),
+(33, 14, 'H3'),
+(34, 15, 'H1'),
+(35, 15, 'H3'),
+(36, 16, 'H1'),
+(37, 16, 'H2'),
+(38, 16, 'H3'),
+(39, 16, 'H4'),
+(40, 17, 'H1'),
+(41, 17, 'H2'),
+(42, 17, 'H3'),
+(43, 18, 'H1'),
+(44, 18, 'H2'),
+(45, 18, 'H3'),
+(46, 19, 'H1'),
+(47, 19, 'H2'),
+(48, 19, 'H3'),
+(49, 20, 'H1'),
+(50, 20, 'H2'),
+(51, 20, 'H3');
 
 -- --------------------------------------------------------
 
@@ -136,7 +297,11 @@ CREATE TABLE `material` (
 
 INSERT INTO `material` (`m_id`, `material`) VALUES
 (1, 'Kaltschaum'),
-(2, 'Federkern');
+(2, 'Federkern'),
+(3, 'Latex'),
+(4, 'Naturlatex'),
+(5, 'Viskose'),
+(6, 'Komfortschaum');
 
 -- --------------------------------------------------------
 
@@ -149,7 +314,6 @@ CREATE TABLE `mattress` (
   `mattress` varchar(255) NOT NULL,
   `price` int(11) NOT NULL,
   `group_fk` int(11) NOT NULL,
-  `data_fk` varchar(50) NOT NULL,
   `material_fk` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -157,28 +321,27 @@ CREATE TABLE `mattress` (
 -- Daten für Tabelle `mattress`
 --
 
-INSERT INTO `mattress` (`ma_id`, `mattress`, `price`, `group_fk`, `data_fk`, `material_fk`) VALUES
-(8, 'Werkmeister M S55 Plus Kaltschaummatratze', 999, 1, 'H1', 1),
-(12, 'Werkmeister M T631 Taschenfederkernmatratze', 1099, 1, 'H2', 2);
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `p_data`
---
-
-CREATE TABLE `p_data` (
-  `d_id` varchar(50) NOT NULL,
-  `haertegrad` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Daten für Tabelle `p_data`
---
-
-INSERT INTO `p_data` (`d_id`, `haertegrad`) VALUES
-('H1', 'soft'),
-('H2', 'medium');
+INSERT INTO `mattress` (`ma_id`, `mattress`, `price`, `group_fk`, `material_fk`) VALUES
+(1, 'Werkmeister M T610 Taschenfederkernmatratze', 949, 1, 2),
+(2, 'Werkmeister M S70 Plus Kaltschaummatratze', 1149, 1, 1),
+(3, 'Werkmeister M S55 Flexo Kaltschaummatratze', 879, 1, 1),
+(4, 'Werkmeister M T631L Taschenfederkernmatratze', 1339, 1, 2),
+(5, 'Werkmeister M S55 Komfort Kaltschaummatratze', 1129, 1, 1),
+(6, 'Werkmeister M S70 Plus WS Kaltschaummatratze', 1149, 1, 1),
+(7, 'dormiente Naturlatexmatratze Natural Eco Plus', 785, 1, 4),
+(8, 'dormiente Naturlatexmatratze Natural Basic 3', 995, 1, 4),
+(9, 'dormiente Naturlatexmatratze Natural Basic 4', 995, 1, 4),
+(10, 'dormiente Naturmatratze Natur Pur Classic Hanf', 1571, 1, 4),
+(11, 'dormiente Naturmatratze Natur Pur Deluxe Seegras', 2095, 1, 4),
+(12, 'selecta K2 Kindermatratze', 399, 2, 6),
+(13, 'Werkmeister YoungStar Kindermatratze', 449, 2, 6),
+(14, 'Baumberger Vario Kindermatratze', 249, 2, 4),
+(15, 'Prolana Kindermatratze Nele Plus', 699, 2, 4),
+(16, 'Werkmeister M S55 Plus Kaltschaummatratze', 999, 1, 1),
+(17, 'Werkmeister M L430 Latexmatratze', 1499, 1, 3),
+(18, 'selecta L4 Latexmatratze', 699, 1, 3),
+(19, 'selecta V6 Visco-Matratze', 799, 1, 5),
+(20, 'Dermapur Feeling 21 Visco-Matratze', 1098, 1, 5);
 
 -- --------------------------------------------------------
 
@@ -198,8 +361,15 @@ CREATE TABLE `size` (
 
 INSERT INTO `size` (`s_id`, `width`, `height`) VALUES
 (1, 80, 200),
-(2, 80, 220),
-(3, 90, 200);
+(2, 90, 200),
+(3, 100, 200),
+(4, 120, 200),
+(5, 140, 200),
+(6, 160, 200),
+(7, 180, 200),
+(8, 200, 200),
+(9, 60, 120),
+(10, 70, 140);
 
 -- --------------------------------------------------------
 
@@ -218,9 +388,150 @@ CREATE TABLE `size_mattress` (
 --
 
 INSERT INTO `size_mattress` (`sm_id`, `ma_id`, `s_id`) VALUES
-(1, 8, 1),
-(2, 12, 3),
-(3, 12, 2);
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 3),
+(4, 1, 4),
+(5, 1, 5),
+(6, 1, 6),
+(7, 1, 7),
+(8, 1, 8),
+(9, 2, 1),
+(10, 2, 2),
+(11, 2, 3),
+(12, 2, 4),
+(13, 2, 5),
+(14, 2, 6),
+(15, 2, 7),
+(16, 2, 8),
+(17, 3, 1),
+(18, 3, 2),
+(19, 3, 3),
+(20, 3, 4),
+(21, 3, 5),
+(22, 3, 6),
+(23, 3, 7),
+(24, 3, 8),
+(25, 4, 1),
+(26, 4, 2),
+(27, 4, 3),
+(28, 4, 4),
+(29, 4, 5),
+(30, 4, 6),
+(31, 4, 7),
+(32, 4, 8),
+(33, 5, 1),
+(34, 5, 2),
+(35, 5, 3),
+(36, 5, 4),
+(37, 5, 5),
+(38, 5, 6),
+(39, 5, 7),
+(40, 5, 8),
+(41, 6, 1),
+(42, 6, 2),
+(43, 6, 3),
+(44, 6, 4),
+(45, 6, 5),
+(46, 6, 6),
+(47, 6, 7),
+(48, 6, 8),
+(49, 7, 1),
+(50, 7, 2),
+(51, 7, 3),
+(52, 7, 4),
+(53, 7, 5),
+(54, 7, 6),
+(55, 7, 7),
+(56, 7, 8),
+(57, 8, 1),
+(58, 8, 2),
+(59, 8, 3),
+(60, 8, 4),
+(61, 8, 5),
+(62, 8, 6),
+(63, 8, 7),
+(64, 8, 8),
+(65, 9, 1),
+(66, 9, 2),
+(67, 9, 3),
+(68, 9, 4),
+(69, 9, 5),
+(70, 9, 6),
+(71, 9, 7),
+(72, 9, 8),
+(73, 10, 1),
+(74, 10, 2),
+(75, 10, 3),
+(76, 10, 4),
+(77, 10, 5),
+(78, 10, 6),
+(79, 10, 7),
+(80, 10, 8),
+(81, 11, 1),
+(82, 11, 2),
+(83, 11, 3),
+(84, 11, 4),
+(85, 11, 5),
+(86, 11, 6),
+(87, 11, 7),
+(88, 11, 8),
+(89, 12, 1),
+(90, 12, 2),
+(91, 12, 3),
+(92, 12, 4),
+(93, 12, 5),
+(94, 13, 1),
+(95, 13, 2),
+(96, 13, 3),
+(97, 14, 2),
+(98, 14, 3),
+(99, 14, 5),
+(100, 14, 9),
+(101, 14, 10),
+(102, 15, 1),
+(103, 15, 2),
+(104, 15, 3),
+(105, 15, 5),
+(106, 16, 1),
+(107, 16, 2),
+(108, 16, 3),
+(109, 16, 4),
+(110, 16, 5),
+(111, 16, 6),
+(112, 16, 7),
+(113, 16, 8),
+(114, 17, 1),
+(115, 17, 2),
+(116, 17, 3),
+(117, 17, 4),
+(118, 17, 5),
+(119, 17, 6),
+(120, 17, 7),
+(121, 17, 8),
+(122, 18, 1),
+(123, 18, 2),
+(124, 18, 3),
+(125, 18, 4),
+(126, 18, 5),
+(127, 18, 6),
+(128, 18, 7),
+(129, 18, 8),
+(130, 19, 1),
+(131, 19, 2),
+(132, 19, 3),
+(133, 19, 4),
+(134, 19, 5),
+(135, 19, 6),
+(136, 19, 7),
+(137, 19, 8),
+(138, 20, 1),
+(139, 20, 2),
+(140, 20, 3),
+(141, 20, 4),
+(142, 20, 5),
+(143, 20, 6),
+(144, 20, 7);
 
 --
 -- Indizes der exportierten Tabellen
@@ -255,10 +566,24 @@ ALTER TABLE `complaints_mattress`
   ADD KEY `ma_id` (`ma_id`);
 
 --
--- Indizes für die Tabelle `group`
+-- Indizes für die Tabelle `groups`
 --
-ALTER TABLE `group`
+ALTER TABLE `groups`
   ADD PRIMARY KEY (`g_id`);
+
+--
+-- Indizes für die Tabelle `haertegrade`
+--
+ALTER TABLE `haertegrade`
+  ADD PRIMARY KEY (`h_id`) USING BTREE;
+
+--
+-- Indizes für die Tabelle `haertegrade_mattress`
+--
+ALTER TABLE `haertegrade_mattress`
+  ADD PRIMARY KEY (`hm_id`),
+  ADD KEY `Schlüssel 2` (`ma_id`),
+  ADD KEY `Schlüssel 3` (`h_id`);
 
 --
 -- Indizes für die Tabelle `material`
@@ -272,14 +597,7 @@ ALTER TABLE `material`
 ALTER TABLE `mattress`
   ADD PRIMARY KEY (`ma_id`),
   ADD KEY `Schlüssel 2` (`material_fk`),
-  ADD KEY `Schlüssel 3` (`group_fk`),
-  ADD KEY `Schlüssel 4` (`data_fk`);
-
---
--- Indizes für die Tabelle `p_data`
---
-ALTER TABLE `p_data`
-  ADD PRIMARY KEY (`d_id`);
+  ADD KEY `Schlüssel 3` (`group_fk`);
 
 --
 -- Indizes für die Tabelle `size`
@@ -303,55 +621,61 @@ ALTER TABLE `size_mattress`
 -- AUTO_INCREMENT für Tabelle `allergy`
 --
 ALTER TABLE `allergy`
-  MODIFY `a_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `a_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT für Tabelle `allergy_mattress`
 --
 ALTER TABLE `allergy_mattress`
-  MODIFY `am_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `am_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT für Tabelle `complaints`
 --
 ALTER TABLE `complaints`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT für Tabelle `complaints_mattress`
 --
 ALTER TABLE `complaints_mattress`
-  MODIFY `cm_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `cm_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
--- AUTO_INCREMENT für Tabelle `group`
+-- AUTO_INCREMENT für Tabelle `groups`
 --
-ALTER TABLE `group`
+ALTER TABLE `groups`
   MODIFY `g_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT für Tabelle `haertegrade_mattress`
+--
+ALTER TABLE `haertegrade_mattress`
+  MODIFY `hm_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT für Tabelle `material`
 --
 ALTER TABLE `material`
-  MODIFY `m_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `m_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT für Tabelle `mattress`
 --
 ALTER TABLE `mattress`
-  MODIFY `ma_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `ma_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT für Tabelle `size`
 --
 ALTER TABLE `size`
-  MODIFY `s_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `s_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT für Tabelle `size_mattress`
 --
 ALTER TABLE `size_mattress`
-  MODIFY `sm_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `sm_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=145;
 
 --
 -- Constraints der exportierten Tabellen
@@ -361,30 +685,36 @@ ALTER TABLE `size_mattress`
 -- Constraints der Tabelle `allergy_mattress`
 --
 ALTER TABLE `allergy_mattress`
-  ADD CONSTRAINT `FK_allergy_mattress_allergy` FOREIGN KEY (`a_id`) REFERENCES `allergy` (`a_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `FK_allergy_mattress_mattress` FOREIGN KEY (`ma_id`) REFERENCES `mattress` (`ma_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `FK_allergy_mattress_allergy` FOREIGN KEY (`a_id`) REFERENCES `allergy` (`a_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_allergy_mattress_mattress` FOREIGN KEY (`ma_id`) REFERENCES `mattress` (`ma_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints der Tabelle `complaints_mattress`
 --
 ALTER TABLE `complaints_mattress`
-  ADD CONSTRAINT `complaints_mattress_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `complaints` (`c_id`),
-  ADD CONSTRAINT `complaints_mattress_ibfk_2` FOREIGN KEY (`ma_id`) REFERENCES `mattress` (`ma_id`);
+  ADD CONSTRAINT `complaints_mattress_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `complaints` (`c_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `complaints_mattress_ibfk_2` FOREIGN KEY (`ma_id`) REFERENCES `mattress` (`ma_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints der Tabelle `haertegrade_mattress`
+--
+ALTER TABLE `haertegrade_mattress`
+  ADD CONSTRAINT `FK_haertegrade_mattress_haertegrade` FOREIGN KEY (`h_id`) REFERENCES `haertegrade` (`h_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_haertegrade_mattress_mattress` FOREIGN KEY (`ma_id`) REFERENCES `mattress` (`ma_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints der Tabelle `mattress`
 --
 ALTER TABLE `mattress`
-  ADD CONSTRAINT `FK_mattress_group` FOREIGN KEY (`group_fk`) REFERENCES `group` (`g_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `FK_mattress_material` FOREIGN KEY (`material_fk`) REFERENCES `material` (`m_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `FK_mattress_p_data` FOREIGN KEY (`data_fk`) REFERENCES `p_data` (`d_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `FK_mattress_group` FOREIGN KEY (`group_fk`) REFERENCES `groups` (`g_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_mattress_material` FOREIGN KEY (`material_fk`) REFERENCES `material` (`m_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints der Tabelle `size_mattress`
 --
 ALTER TABLE `size_mattress`
-  ADD CONSTRAINT `size_mattress_ibfk_1` FOREIGN KEY (`ma_id`) REFERENCES `mattress` (`ma_id`),
-  ADD CONSTRAINT `size_mattress_ibfk_2` FOREIGN KEY (`s_id`) REFERENCES `size` (`s_id`);
+  ADD CONSTRAINT `size_mattress_ibfk_1` FOREIGN KEY (`ma_id`) REFERENCES `mattress` (`ma_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `size_mattress_ibfk_2` FOREIGN KEY (`s_id`) REFERENCES `size` (`s_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
